8d40774ab19c9cdc8769a0a2630e65f4bf2b6bd0
Allison Schambers <aschambers4291@gmail.com>
2021-02-14 14:36:37 -0800

    update port

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(5000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 4000, function () {
+  console.log('Example app listening on port 4000!');
 });
